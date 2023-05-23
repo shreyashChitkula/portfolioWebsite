a367ea52b2d4a5997ca12a17454a73a76002a9af
shreyashChitkula <ro200020@rguktong.ac.in>
2023-05-23 11:25:42 +0530

    pushing the local code into the github

---
+++ absolutePages/strong_numbersS_till_n.py
@@ -0,0 +1,22 @@
+# n=int(input("enter a number"))
+# for m in range(1,n+1):
+#     k=m
+#     x=0
+#     while m!=0:
+#         a=m%10
+#         b=1
+#         for i in range(a,0,-1):
+#             b=b*i
+#         x=x+b
+#         m=m//10
+#     if x==k:
+#         print(k)
+# else:
+#   print("not a strong")
+
+
+# k=int(input("enter a number? "))
+# p=len(str(k))
+# print(p)
+# print(type(k))
+
